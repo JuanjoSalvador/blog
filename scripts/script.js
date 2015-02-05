600e0d24f42b1a35ecfc4ebdd324086fec0d1461
Juan José Salvador <juanjosalvador@openmailbox.org>
2015-02-05 18:45:14 +0100

    Update 5/2/15
    Modificados algunos fallos, pendiente de añadir el "Mostrar más" y
    añadido el test chorra.

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+<!--  
+document.write('<style type="text/css">div.cp_oculta{display: none;}</style>');  
+function MostrarOcultar(capa,enlace)  
+{  
+    if (document.getElementById)  
+    {  
+        var aux = document.getElementById(capa).style;  
+        aux.display = aux.display? "":"block";  
+    }  
+}  
+   
+//-->  
