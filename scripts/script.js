9451f15ef5a0ff211c6945c29d50ab4b0c6b5f57
JuanjoSalvador <juanjosalvador@openmailbox.org>
2015-03-16 17:02:55 +0100

    Actualización 0.2

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,20 @@
-<!--  
-document.write('<style type="text/css">div.cp_oculta{display: none;}</style>');  
-function MostrarOcultar(capa,enlace)  
-{  
-    if (document.getElementById)  
-    {  
-        var aux = document.getElementById(capa).style;  
-        aux.display = aux.display? "":"block";  
-    }  
-}  
-   
-//-->  
+<!--
+document.write('<style type="text/css">div.cp_oculta{display: none;}</style>');
+function MostrarOcultar(capa,enlace)
+{
+    if (document.getElementById)
+    {
+        var aux = document.getElementById(capa).style;
+        aux.display = aux.display? "":"block";
+    }
+}
+
+function navbar() {
+  titulo = document.title;
+  if (titulo = 'JuanjoSalvador.es') {
+    var home = document.getElementById(home).style;
+    home.font-color = home.font-color? "":"#f24537";
+  }
+}
+
+//-->
