0b6439a40599aefb249c913f6f2e70baeae3dec9
JuanjoSalvador <juanjosalvador@openmailbox.org>
2017-06-23 16:54:57 +0200

    updated test, now with picnic css

---
+++ random/test/script.js
@@ -0,0 +1,17 @@
+function contar() {
+    var checkboxes = document.getElementById("test").checkbox; //Array que contiene los checkbox
+    var cont = 0; //Variable que lleva la cuenta de los checkbox pulsados
+    for (var x=0; x < checkboxes.length; x++) {
+        if (checkboxes[x].checked) {
+            cont = cont + 1;
+        }
+    }
+    
+    var porcen = (cont/checkboxes.length)*100;
+    
+    if (cont == checkboxes.length) {
+        alert("ZOMG. Eres ELLA.");
+    } else {
+        alert("Resultado: " + cont + "/" + checkboxes.length + ". Eso supone un " + porcen + "% de posibilidades de que seas ELLA.");
+    }
+}
