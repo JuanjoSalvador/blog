4d8cc1c3a5d3406d9c74cf9e5c50e7df42f865bb
JuanjoSalvador <juanjosalvador@openmailbox.org>
2017-06-23 19:28:29 +0200

    added more functions and fixed styles

--- random/test/script.js
+++ random/test/script.js
@@ -1,4 +1,6 @@
-function contar() {
+var porcen = null;
+
+function contar() {    
     var checkboxes = document.getElementById("test").checkbox; //Array que contiene los checkbox
     var cont = 0; //Variable que lleva la cuenta de los checkbox pulsados
     for (var x=0; x < checkboxes.length; x++) {
@@ -7,15 +9,29 @@ function contar() {
         }
     }
     
-    var porcen = (cont/checkboxes.length)*100;
+    porcen = Math.round((cont/checkboxes.length)*100);
+    var box = document.getElementById('modal').children[0].children[1];
+
+    console.log(box);
     
     if (cont == checkboxes.length) {
-        alert("ZOMG. Eres ELLA.");
+        box.innerHTML = "ZOMG. Eres ELLA.";
     } else {
-        alert("Resultado: " + cont + "/" + checkboxes.length + ". Eso supone un " + porcen + "% de posibilidades de que seas ELLA.");
+        box.innerHTML = "Resultado: " + cont + "/" + checkboxes.length + ". Eso supone un " + porcen + "% de posibilidades de que seas ELLA.";
     }
+
+    document.getElementById('modal').style.display = 'block';
 }
 
 function hi() {
     window.open('https://twitter.com/intent/tweet?text=¡Hola @HighLordIron!','Saludar','width=600,height=350');
+}
+
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+}
+
+function share() {
+    let str = encodeURI("Hola @HighLordIron, he sacado un " + porcen + "% en tu test de 'La Mujer Perfecta'. - http://juanjosalvador.me/random/test/");
+    window.open('https://twitter.com/intent/tweet?text=' + str,'Saludar','width=600,height=350');
 }
